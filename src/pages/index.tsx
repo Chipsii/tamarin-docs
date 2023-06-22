ecb416ea44b0c8bab3e49f1e8c59a37c0a3e8191
towhid <ktowhid20@gmail.com>
2023-06-22 12:14:55 +0600

    added mdx support

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Scripts from './scripts.mdx';
+ 
+export const meta = {
+  author: 'Rich Haines',
+};
+ 
+function Index() {
+//   <MyLayoutComponent meta={meta}><HelloWorld /></MyLayoutComponent>
+  return <div> <Scripts/> </div>
+}
+
+export default Index;
