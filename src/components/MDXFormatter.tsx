61d0e57a19b4f2353f356de418769ca3cf82c7a2
towhid <ktowhid20@gmail.com>
2023-06-22 13:19:19 +0600

    added mdx styling ref

--- src/components/MDXFormatter.tsx
+++ src/components/MDXFormatter.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function MDXFormatter({children}:any) {
     return (
+        //ref: https://tailwindcss.com/docs/typography-plugin for styling
         <article className="prose prose-zinc prose-headings:text-white lg:prose-xl">
         {children}
       </article>
