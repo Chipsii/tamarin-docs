c58830291f4116ac693b4324d10c11cf56ebbcf2
Hasib <hasibfire24@gmail.com>
2023-06-24 01:22:34 +0600

    Keeping it simple for now

--- src/components/MDXFormatter.tsx
+++ src/components/MDXFormatter.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MDXFormatter({children}:any) {
     return (
         //ref: https://tailwindcss.com/docs/typography-plugin for styling
-        <article className="prose prose-zinc prose-headings:text-white lg:prose-xl">
+        <article className="prose prose-zinc prose-headings:text-white prose-p:text-gray-400 prose-p:text-lg prose-li:text-xl prose-li:text-cyan-400 prose-strong:text-xl prose-strong:text-violet-600 lg:prose-xl">
         {children}
       </article>
     )
